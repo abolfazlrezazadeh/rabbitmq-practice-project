ba0b2d4ace7774d703f280c5ab7f43f7640dddb0
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-13 12:28:51 +0330

    adding and learn some options

--- consumer.js
+++ consumer.js
@@ -1,11 +1,15 @@
-const amqp = require("amqplib")
-
-async function receiveFromProducer(){
-    const queueName = 'service1'
-    const connection = await amqp.connect("amqp://localhost:5672")
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queueName,{durable : true})
-    channel.consume(queueName,message => console.log(message.content.toString()))
+const amqp = require("amqplib");
+let index = 0;
+async function receiveFromProducer() {
+  const queueName = "service1";
+  const connection = await amqp.connect("amqp://localhost:5672");
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queueName, { durable: true });
+  channel.consume(queueName, (message) => {
+    console.log(`${index} : `, message.content.toString());
+    index++;
+  },{noAck : true});
+  // noAck = confirm requests that received and delete them from queue
 }
 
-receiveFromProducer()
+receiveFromProducer();
--- producer.js
+++ producer.js
@@ -1,11 +1,24 @@
-const amqp = require("amqplib")
+const amqp = require("amqplib");
 
-async function connectToService1(){
-    const queueName = 'service1'
-    const connection = await amqp.connect("amqp://localhost:5672")
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queueName,{durable : true})
-    channel.sendToQueue(queueName,Buffer.from("hello rabbitMq"))
+async function connectToService1() {
+  const queueName = "service1";
+  const connection = await amqp.connect("amqp://localhost:5672");
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queueName);
+  channel.sendToQueue(
+    queueName,
+    Buffer.from("hello rabbitMq"),
+    {
+      persistent: true,
+    } /* presistant = if server is down write the requjests in disk */
+  );
+  console.log("message send to consumer");
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 1000);
 }
 
-connectToService1()
+for (let index = 0; index < 20; index++) {
+  connectToService1();
+}
