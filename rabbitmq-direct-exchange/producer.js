6241508fcb56ff79af9087182c5336789c345503
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-14 17:05:06 +0330

    copnfig consumer file

--- rabbitmq-direct-exchange/producer.js
+++ rabbitmq-direct-exchange/producer.js
@@ -1,11 +1,11 @@
 const amqp = require("amqplib");
-const exchangeName = "direct";
-
+const exchangeName = "directMessage";
+const [logType, message] = process.argv.slice(2); // pattern and message are come from terminal
 async function sendMessage() {
   const connection = await amqp.connect("amqp://localhost:5672");
   const channel = await connection.createChannel();
   await channel.assertExchange(exchangeName, "direct" /*,{durable : true}*/);
-  channel.publish(exchangeNamem, "error", Buffer.from("hello "));
+  channel.publish(exchangeName, logType, Buffer.from(message));
   setTimeout(() => {
     process.exit(0);
   });
