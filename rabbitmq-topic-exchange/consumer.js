34c813c336d20414c212016bcde09604de3ad97e
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-17 12:43:44 +0330

    last commit

--- rabbitmq-topic-exchange/consumer.js
+++ rabbitmq-topic-exchange/consumer.js
@@ -1,12 +1,12 @@
 const amqp = require("amqplib");
-const exchangeName = "directMessage";
+const exchangeName = "topicMessage";
 const logType = process.argv.slice(2)
 // logtype =  info warnnig error
 console.log(logType);
 async function recieveMessage() {
   const connection = await amqp.connect("amqp://localhost:5672");
   const channel = await connection.createChannel();
-  await channel.assertExchange(exchangeName, "direct");
+  await channel.assertExchange(exchangeName, "topic");
   const assertedQueue = await channel.assertQueue("", { exclusive: true });
   for (const pattern of logType) {
       channel.bindQueue(assertedQueue.queue, exchangeName, pattern); 
