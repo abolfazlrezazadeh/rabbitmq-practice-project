884981cf73d505aeb7b7d620245c36609feb5f2f
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-17 12:29:45 +0330

    config  producer file

---
+++ rabbitmq-topic-exchange/producer.js
@@ -0,0 +1,14 @@
+const amqp = require("amqplib");
+const exchangeName = "topicMessage";
+const [logType, message] = process.argv.slice(2); // pattern and message are come from terminal
+async function sendMessage() {
+  const connection = await amqp.connect("amqp://localhost:5672");
+  const channel = await connection.createChannel();
+  await channel.assertExchange(exchangeName, "topic" /*,{durable : true}*/);
+  channel.publish(exchangeName, logType, Buffer.from(message));
+  setTimeout(() => {
+    process.exit(0);
+  });
+}
+
+sendMessage();
